18e993362e058e58425859922476e82ee8e5205a
Manuel Rodriguez <manuel.rodriguez@bookandsmile.com>
2015-11-08 23:09:46 +0100

    Open url in browser when running gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
+'use strict';
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var plumber = require('gulp-plumber');
+var open = require('gulp-open');
 
-gulp.task('default', ['compress', 'watch']);
+gulp.task('default', ['compress', 'openUrl', 'watch']);
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('src/*.js', ['compress']);
 });
 
-gulp.task('compress', function() {
+gulp.task('openUrl', function () {
+  return gulp.src('')
+    .pipe(open({uri: 'http://localhost:4000'}));
+});
+
+gulp.task('compress', function () {
   return gulp.src('src/**/*.js')
     .pipe(plumber())
     .pipe(jshint())
